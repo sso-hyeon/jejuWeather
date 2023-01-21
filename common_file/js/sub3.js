9320b611d1dc7b5eea894ca68aa0fca64ed1319f
sso-hyeon <ashash1994@naver.com>
2023-01-22 00:39:33 +0900

    Feat: 한라산 입하산허용시간 API 데이터 변동으로 인한 UI 수정

--- common_file/js/sub3.js
+++ common_file/js/sub3.js
@@ -1,48 +1,36 @@
-document
-  .querySelector(".course-name-wrap")
-  .addEventListener("click", function (e) {
-    if (!e.target.classList.contains("course-btn")) return;
-    btnReset(".course-btn");
-    e.target.classList.add("on");
-    document.querySelector("#course-info h3").innerText = e.target.innerText;
-    document.querySelector("#course-info").classList.remove("d-none");
-    courseListLoad(e.target.dataset.name);
-  });
+document.querySelector(".course-name-wrap").addEventListener("click", function (e) {
+  if (!e.target.classList.contains("course-btn")) return;
+  btnReset(".course-btn");
+  e.target.classList.add("on");
+  document.querySelector("#course-info h3").innerText = e.target.innerText;
+  document.querySelector("#course-info").classList.remove("d-none");
+  courseListLoad(e.target.dataset.name);
+  document.querySelector("#info-list-wrap").innerHTML = "";
+});
 
 function courseListLoad(courseName) {
   fetch(MOUNTINE_API)
     .then(response => response.json())
     .then(result => {
-      const courseList = result.data.filter(
-        list => list["탐방로명"] === courseName
-      );
-      createCourseInfoList(courseList[0]);
+      const courseList = result.data.filter(list => list["탐방로별"] === courseName);
+      courseList.forEach(course => {
+        createCourseInfoList(course);
+      });
     });
 }
 
 function createCourseInfoList(list) {
-  const infoList = document.querySelector(".info-list");
-  infoList.innerHTML = "";
+  const infoList = document.createElement("ul");
+  infoList.className = "info-list";
   const winter = document.createElement("li");
-  winter.innerHTML =
-    "입산시간: " +
-    list["동절기 입산시간"] +
-    "<br />제한시간: " +
-    list["동절기 입산제한시간"];
+  winter.innerHTML = list["동절기"];
   const springNfall = document.createElement("li");
-  springNfall.innerHTML =
-    "입산시간: " +
-    list["춘추절기 입산시간"] +
-    "<br />제한시간: " +
-    list["춘추절기 입산제한시간"];
+  springNfall.innerHTML = list["춘추절기"];
   const summer = document.createElement("li");
-  summer.innerHTML =
-    "입산시간: " +
-    list["하절기 입산시간"] +
-    "<br />제한시간: " +
-    list["하절기 입산제한시간"];
+  summer.innerHTML = list["하절기"];
   const startPoint = document.createElement("li");
-  startPoint.innerText = list["입산제한 기준점"];
+  startPoint.innerText = list["통제장소"];
 
   infoList.append(winter, springNfall, summer, startPoint);
+  document.querySelector("#info-list-wrap").appendChild(infoList);
 }
