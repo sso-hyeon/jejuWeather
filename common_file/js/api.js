6c0a8b26e7e19cd8d2b7e43b963e5475d0221b6a
sso-hyeon <ashash1994@naver.com>
2023-04-20 15:45:54 +0900

    fix: 한라산 입하산 허용시간 API KEY 변경

--- common_file/js/api.js
+++ common_file/js/api.js
@@ -2,7 +2,7 @@ const API_KEY = "3459d3dc26c89178443519f7030ecb86";
 const WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/";
 
 const MOUNTINE_KEY =
-  "ScbK84OkZ1IZzox%2BPKb3jyJIRVqZlHnuVi7BHlRseYuHMjoScTcMZHhwM%2BvjxvUgCaQU%2F9072H%2BFbPv8cr01SA%3D%3D";
+  "c%2BsWtsSpX1j%2BdW4NhBiCB2%2FE4iroRivmTs6ckJFeFU73XWr9tH73EFaKQPPg77A22RZIX%2FoeEuiAQnpqMn89qg%3D%3D";
 const MOUNTINE_API =
   "https://api.odcloud.kr/api/15056441/v1/uddi:1635e441-a1c9-42c1-95a8-a2b4891245bb_201911141055?serviceKey=" +
   MOUNTINE_KEY;
